22f21d60b529917b15b5070f1fa4fba1f041cd18
Trang <nguyenthuytrang.fr@gmail.com>
2024-02-26 14:39:03 +0100

    ...

--- bonus2.py
+++ bonus2.py
@@ -2,13 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def histogram():
-    df = pd.read_csv('your_data.csv')
-    avg_price_in_category = df['price_including_tax']
+    df = pd.read_csv('books_details_by_category.csv')
+    df.set_index('category', inplace=True)
+    avg_price_in_category = df['avg_price_in_category']
+    avg_price_in_category_sorted = avg_price_in_category.sort_values()
     plt.figure(figsize=(10, 6))
-    plt.bar(avg_price_in_category.index, avg_price_in_category.values)
+    plt.bar(avg_price_in_category_sorted.index, avg_price_in_category_sorted.values)
     plt.xlabel('Category')
     plt.ylabel('Average Price')
     plt.title('Average Book Prices by Category')
-    plt.xticks(rotation=45, ha='right')  # Rotate category labels for better readability
-    plt.tight_layout()  # Adjust layout to prevent clipping of labels
+    plt.yticks(fontsize = 6)
+    plt.xticks(rotation=45, ha='right', fontsize=8) 
+    plt.tight_layout() 
+    plt.savefig('histogramme_bonus2.png') 
     plt.show()  
+
+histogram()
